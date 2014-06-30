849aa7939eb52d71f25cff352c51329b1f581273
huangpanxx <ls.snail@gmail.com>
2014-06-30 16:00:24 +0800

    update

---
+++ ia/merge_sort.cpp
@@ -0,0 +1,14 @@
+#include "template.h"
+
+int arr[] = {3,2,1,3,5};
+
+void merge_sort(int *p,int n){
+	
+}
+
+int main() {
+	merge_sort(arr,n);
+	FO(i,SIZE(arr)) {
+		printf("%d\n",arr[i])
+	}
+}
---
+++ ia/template.h
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <ctime>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <utility>
+#include <queue>
+#include <string>
+#include <stack>
+#include <list>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+using namespace std;
+
+#define FO(I,N) for (int I=0;I<(int)(N);++I)
+#define FO_(I,A,B) for (int I=(A);I<(int)(B);++I)
+#define LOOP(n) for (int _llllllll=0;_llllllll<(n);++_llllllll)
+#define SIZE(n) (sizeof(n)/sizeof((n)[0])
+
+
